70bc1d854edc317c092c8aabab4a4639ca1b584c
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2021-12-14 17:16:18 -0500

    Update Simple_Coin.sol

--- Simple_Coin.sol
+++ Simple_Coin.sol
@@ -1,72 +1,66 @@
-pragma solidity^0.5.1;
+pragma solidity ^ 0.8.4;
 
-contract SimpleCoin is Ownable{
-    // getting each users balance in wei.
+contract SimpleCoin{
 
-address public Owner;
-mapping(address => uint256)balances;
-mapping(address => bool)frozen;
+mapping(address => uint) balances;
 
-    uint amount;
-constructor(uint _tokensupply) public {
-Owner = msg.sender;
 
-mint(Owner,_tokensupply);
+event Transfer(address indexed from , address indexed to,uint value);
+
+address owner;
+constructor (uint initialsupply) public {
+owner = msg.sender;
+ balances[owner] = initialsupply;
 }
 
-event Transfer(address indexed from, address indexed to, uint value);
-event Freezing(address target, bool frozen);
+
+
+function transfer(address to, uint amount)public{
+
+require(amount > balances[msg.sender]);
+// making the transfer
+
+balances[msg.sender] -= amount;
+balances[to] += amount;
+
+emit Transfer(msg.sender,to,amount);
+}
+
+
+mapping(address=>mapping(address=>uint)) allowance;
+
+function authorize(address account,uint maxamount)public{
+    allowance[msg.sender][account] = maxamount;
+}
+
+function transfallowance(address from, uint amount,address dest)public OnlyOwner{
+   // require( dest != 0x0);
+   require(allowance[from][msg.sender] != 0);
+   require(amount <= balances[msg.sender]);
+   balances[from] -= amount;
+   balances[dest] += amount;
+   allowance[from][msg.sender] -= amount;
+
+emit Transfer(from,dest,amount);
+}
+
+// minting coins
+function mint(uint amount,address receiver) public OnlyOwner{
+
+balances[receiver] += amount;
+emit Transfer(msg.sender,receiver,amount);
+
+}
 
 modifier OnlyOwner{
-    if(msg.sender != Owner) revert(); _;
+    require(msg.sender == owner); _;
 }
 
-modifier Check_balance{
-        require(balances[msg.sender] >= amount,"Not enough funds :("); _;
-    }
+mapping(address=>bool) frozen;
 
-function transfer(address _to,uint _amount)public payable Check_balance{
-
-        balances[_to] += _amount;
-        balances[msg.sender] -= _amount;
-        emit Transfer(msg.sender,_to,_amount);
-    }
-    // Authorize an allowance to another account.
-mapping(address=>mapping(address=>uint)) public allowance;
-
-function Authorize_account(address _authorized, uint _amount)public returns(bool success){
-allowance[msg.sender][_authorized] = _amount;
-return true;
-}
-// allows transfering auth amount.
-
-function TransferAmount(address _from, address _to,uint256 _amount)public returns(bool success){
-    require(_to != 0x0,"Address is empty");
-    require(balances[_from] > amount,"Not enough funds!");
-    // check allowance isn't exceeded.
-    require(_amount < allowance[_from][msg.sender],"Allowance exceeded");
-balances[_from] -= _amount;
-balances[_to] += _amount;
-allowance[_from][msg.sender] -= _amount;
-
-
-emit Transfer(_from,_to,_amount);
-return true;
-
-}
-
-// mint function.
-
-function mint(address _recipient, uint _m_amount)public OnlyOwner {
-
-balances[_recipient] = _m_amount;
-
-emit Transfer(Owner, _recipient,_m_amount);
-
-}
-
-function Freeze_Account(address target, bool freeze)public OnlyOwner{
+function freeze(address target )public OnlyOwner{
     frozen[target] = true;
-    emit Freezing(target,true);
+    
 }
+
 }
